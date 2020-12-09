370fb97c3e0979f4dd1e2fd57443e6778322d372
RurouniKenshin24 <ogzhn_9225@hotmail.com>
2020-12-08 18:26:00 -0600

    Initial Commit

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+
+    @Test
+    public void test1(){
+        Assertions.assertEquals(4,1+3);
+    }
+}

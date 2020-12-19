8928fc41c3ca654edac67727954da1ded0fc866a
RurouniKenshin24 <ogzhn_9225@hotmail.com>
2020-12-19 01:27:11 -0600

    Add new test on HR_ORDS_API_DB_Test.java

--- src/test/java/day8/HR_ORDS_API_DB_Test.java
+++ src/test/java/day8/HR_ORDS_API_DB_Test.java
@@ -1,5 +1,6 @@
 package day8;
 
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pojo.hr.Region;
@@ -42,5 +43,38 @@ public class HR_ORDS_API_DB_Test extends HR_ORDS_TestBase {
 
         assertThat(region.getRegion_id() + "",equalTo(thirdRow.get("REGION_ID")));
         assertThat(region.getRegion_name() + "",equalTo(thirdRow.get("REGION_NAME")));
+    }
+
+    @DisplayName("Testing GET/regions/{region_id} Result match with Database value by value")
+    @Test
+    public void testDB_API_ResultsAsJson(){
+
+        Response response = given()
+                .pathParam("region_id",3).
+                        when()
+                .get("/regions/{region_id}").
+                        then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .response();
+
+//        DB_Utility.runQuery("select * from regions");
+//        Map<String,String> thirdRow = DB_Utility.getRowMap(3);
+
+//        assertThat(region_id,is(thirdRow.get("REGION_ID")));
+//        assertThat(region_name,is(thirdRow.get("REGION_NAME")));
+
+        String region_id = "" + response.jsonPath().getInt("region_id");
+        String region_name = response.jsonPath().getString("region_name");
+
+        int myId = 3;
+        DB_Utility.runQuery("select region_id,region_name from regions where region_id = " + myId);
+        String expectedRegion_id = DB_Utility.getColumnDataAtRow(1,1);
+        String expectedRegion_Name = DB_Utility.getColumnDataAtRow(1,2);
+
+        assertThat(region_id,is(expectedRegion_id));
+        assertThat(region_name,is(expectedRegion_Name));
+
     }
 }

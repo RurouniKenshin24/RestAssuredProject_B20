9d818d210181789b29e0acfc568953846119c780
RurouniKenshin24 <ogzhn_9225@hotmail.com>
2020-12-20 23:32:53 -0600

    add new RepeatedTest.java & JUnit5_ParameterizedTest.java classes

--- src/test/java/day9/JUnit5_ParameterizedTest.java
+++ src/test/java/day9/JUnit5_ParameterizedTest.java
@@ -1,6 +1,7 @@
 package day9;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -8,10 +9,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class JUnit5_ParameterizedTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {1,2,3,4,5,6})
-    public void test1(int number){
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    public void test1(int number) {
         System.out.println(number);
 
         assertTrue(number < 10);
+    }
+
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/zipcode.csv", numLinesToSkip = 1)
+    public void test2(String zip) {
+        System.out.println(zip);
     }
 }
